2a8a6a5668199924e182d8b80ce79150aa1aee45
davidgeorgebell <bell.dave93@gmail.com>
2019-06-11 14:37:35 +0400

    Delete function and strikethrough function

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
 const enterBtn = document.getElementById('enter');
 const userInput = document.getElementById('userInput');
-let shoppingList = document.getElementById('shoppingList');
+const shoppingList = document.getElementById('shoppingList');
+const li = document.getElementsByTagName('li');
+const ul = document.querySelector('ul');
 
 
 // Find length of word typed in input box
-function inputLength() {
+const inputLength = () => {
 	return userInput.value.length;
 }
 
 //Check if there is a value in the input box after clicking button
-function addListAfterClick() {
-	if(inputLength() > 0) {
+const addListAfterClick = () => {
+	if (inputLength() > 0) {
 		addItem();
 	}
 }
 
 // Check if the correct keycode has been pressed and if there is a value in the input box
-function addAfterKeypress(e) {
-	if(inputLength() > 0 && e.keyCode === 13) {
+const addAfterKeypress = (e) => {
+	if (inputLength() > 0 && e.keyCode === 13) {
 		addItem();
 	}
 }
 
 //Creates li element, adds the userInput to the li element, appends li to shopping list, clears the input box after
-  const addItem = () => {
-    let li = document.createElement('li');
+const addItem = () => {
+	const li = document.createElement('li');
 	li.appendChild(document.createTextNode(userInput.value));
 	shoppingList.appendChild(li);
 	userInput.value = "";
 
-
-// Create Delete button
-let deleteBtn = document.createElement('button');
-deleteBtn.className = 'delete';
-deleteBtn.appendChild(document.createTextNode('X'));
-li.appendChild(deleteBtn);
+	// Create Delete button
+	const deleteBtn = document.createElement('button');
+	deleteBtn.className = 'delete';
+	deleteBtn.appendChild(document.createTextNode('X'));
+	li.appendChild(deleteBtn);
 }
 
+// Remove item from list when delete button is pressed
+const removeFromList = (e) => {
+	if (e.target.classList.contains('delete')) {
+		let li = e.target.parentElement;
+		shoppingList.removeChild(li);
+
+	}
+}
+
+const lineThrough = (e) => {
+	if (e.target.tagName === 'li');
+	e.target.classList.toggle("done");
 
 
+}
 
-
+ul.addEventListener('click', lineThrough);
+shoppingList.addEventListener('click', removeFromList);
 enterBtn.addEventListener('click', addListAfterClick);
-
 userInput.addEventListener('keypress', addAfterKeypress);
-
